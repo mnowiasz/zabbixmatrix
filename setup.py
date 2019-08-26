19818fa685f13613ead98dc56cd11b0a96874b18
Mark Nowiasz <buckaroo+github@midworld.de>
2019-08-26 17:05:39 +0200

    Argv[1] contains the list of rooms (instead of having them in the config file)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zabbixmatrix',
-    version='0.0.1',
+    version='0.0.2',
     packages=['zabbix2matrix'],
     url='https://github.com/mnowiasz/zabbixmatrix',
     license='MIT',
